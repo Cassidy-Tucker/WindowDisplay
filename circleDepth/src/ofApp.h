aa77ace1b4817b8e025ba5a113122ef3698c8958
Matt Sutton <mcsutton@outlook.com>
2018-02-20 18:57:35 -0700

    added experiments

---
+++ circleDepth/src/ofApp.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxKinectV2.h"
+#include "ofxGui.h"
+
+class ofApp : public ofBaseApp{
+
+	public:
+		void setup();
+		void update();
+		void draw();
+
+		void keyPressed(int key);
+		void keyReleased(int key);
+		void mouseMoved(int x, int y );
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void mouseEntered(int x, int y);
+		void mouseExited(int x, int y);
+		void windowResized(int w, int h);
+		void dragEvent(ofDragInfo dragInfo);
+		void gotMessage(ofMessage msg);
+		
+        ofImage depth_image;
+        ofImage rgb_image;
+    
+        int depth_width, depth_height;
+    vector<ofVec4f> circles;
+    vector<ofColor> colors;
+    
+    ofEasyCam easycam;
+    
+    ofxKinectV2 kinect;
+    
+    ofxPanel gui;
+    ofParameter<float> radius, max_depth, min_depth, zoom;
+    
+};
